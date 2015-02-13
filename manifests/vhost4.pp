c0f9fceefdff99fcb75f74454de3e434e091fe20
Patrick Kelso <psk@puppetlabs.com>
2015-02-13 15:44:25 +1100

    vhosts

---
+++ manifests/vhost4.pp
@@ -0,0 +1,27 @@
+class profiles::vhost4 {
+
+ ## Hiera lookups
+  $vhost_fqdn           = hiera('profiles::vhost4::vhost_fqdn')
+  $vhost_docroot        = hiera('profiles::vhost4::vhost_docroot')
+  $vhost_port           = hiera('profiles::vhost4::vhost_port')
+  $www_user             = hiera('profiles::apache::www_user')
+  $www_group             = hiera('profiles::apache::www_group')
+
+file { "$vhost_docroot":
+  ensure  =>  directory,
+  owner   =>  "$www_user",
+  group   =>  "$www_group",
+  mode    =>  0755,
+}
+
+apache::vhost { "$vhost_fqdn":
+  port    => "$vhost_port",
+  docroot => "$vhost_docroot",
+}
+
+file { "$vhost_docroot/index.html":
+  ensure  =>  file,
+  content =>  "$vhost_fqdn",
+}
+
+}
